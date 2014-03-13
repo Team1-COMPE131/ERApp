84bd3ceff56d531d3ec4679368aa1f1273b283e0
Kevin Jung <kevin.jung@qmiapp.com>
2014-03-13 16:51:51 -0700

    Initial commit

---
+++ ERApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ERApp
+//
+//  Created by Kevin Jung on 3/13/14.
+//  Copyright (c) 2014 Kevin Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
